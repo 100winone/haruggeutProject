db2c3ef151ec3e06dcee78f13948b72a0a677175
100winone <53801551+100winone@users.noreply.github.com>
2020-04-23 17:02:10 +0900

    Until API NO.3
    2020-04-23

--- controllers/DailyController.php
+++ controllers/DailyController.php
@@ -9,11 +9,16 @@ $req = json_decode(file_get_contents("php://input"));
 try {
     addAccessLogs($accessLogs, $req);
     switch ($handler) {
+        /*
+         * API No. 2 ('POST', '/plans)
+         * API Name : 일정입력 API
+         * 마지막 수정 날짜 : 20.04.22
+         */
 
         case "createPlan":
-            http_response_code(201);
+            http_response_code(200);
             $colorId = $req->colorId;
-            $location = $req->location;
+            $place = $req->place;
             $contents = $req->contents;
             $startTime = $req->startTime;
             $endTime = $req->endTime;
@@ -34,13 +39,42 @@ try {
             $userInfo = getDataByJWToken($jwt, JWT_SECRET_KEY);
             $kakaoId = $userInfo->kakaoId;
 
-            createPlan($kakaoId, $colorId, $location, $contents, $startTime, $endTime, $scheduleDate, $title);
+            createPlan($kakaoId, $colorId, $place, $contents, $startTime, $endTime, $scheduleDate, $title);
             $res->isSuccess = TRUE;
             $res->code = 100;
             $res->message = "일정 생성 성공";
             echo json_encode($res, JSON_NUMERIC_CHECK);
             break;
 
+        /*
+         * API No. 3 ('GET', '/plans)
+         * API Name : 일정조회 API
+         * 마지막 수정 날짜 : 20.04.23
+         */
+        case "plans":
+            http_response_code(200);
+
+            $jwt = $_SERVER["HTTP_X_ACCESS_TOKEN"]; // jwt
+
+            if (!isValidHeader($jwt, JWT_SECRET_KEY)) {
+                $res->isSuccess = FALSE;
+                $res->code = 201;
+                $res->message = "유효하지 않은 토큰입니다.";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            $userInfo = getDataByJWToken($jwt, JWT_SECRET_KEY);
+            $kakaoId = $userInfo->kakaoId;
+
+            $scheduleDate = $_GET['scheduleDate'];
+
+            $res->result = plans($kakaoId, $scheduleDate);
+            $res->isSuccess = TRUE;
+            $res->code = 100;
+            $res->message = "일정 조회 성공";
+            echo json_encode($res, JSON_NUMERIC_CHECK);
+            break;
     }
 } catch (\Exception $e) {
     return getSQLErrorException($errorLogs, $e, $req);
--- pdos/DailyPdo.php
+++ pdos/DailyPdo.php
@@ -1,17 +1,52 @@
 <?php
 
-function createPlan($kakaoId, $colorId, $location, $contents, $startTime, $endTime, $scheduleDate, $title)
+function createPlan($kakaoId, $colorId, $place, $contents, $startTime, $endTime, $scheduleDate, $title)
 {
 
     $pdo = pdoSqlConnect();
 
-    $query = "INSERT INTO DAILY_TB (kakaoId, colorId, location, contents, startTime, endTime, scheduleDate, title)
+    $query = "INSERT INTO PLANS_TB (kakaoId, colorId, place, contents, startTime, endTime, scheduleDate, title)
               VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
     $st = $pdo->prepare($query);
-    $st->execute([$kakaoId, $colorId, $location, $contents, $startTime, $endTime, $scheduleDate, $title]);
+    $st->execute([$kakaoId, $colorId, $place, $contents, $startTime, $endTime, $scheduleDate, $title]);
 
     $st = null;
     $pdo = null;
 
+}
+
+function plans($kakaoId, $scheduleDate){
+    $pdo = pdoSqlConnect();
+    $query = "";
+    if($scheduleDate == null){
+        $query = "SELECT @ROWNUM := @ROWNUM + 1 AS NO, A.* FROM
+        (SELECT colorId, emotionId, isPriority, startTime, place, scheduleDate
+           FROM PLANS_TB
+          WHERE scheduleDate = DATE(NOW()) AND kakaoId = ?
+          ORDER BY startTime DESC) A,
+        (SELECT @ROWNUM := 0 ) B";
+
+        $st = $pdo->prepare($query);
+        $st->execute([$kakaoId]);
+    }
+    else if ($scheduleDate != null){
+        $query = "SELECT @ROWNUM := @ROWNUM + 1 AS NO, A.* FROM
+        (SELECT colorId, emotionId, isPriority, startTime, place, scheduleDate
+           FROM PLANS_TB
+          WHERE scheduleDate = ? AND kakaoId = ?
+          ORDER BY startTime DESC) A,
+        (SELECT @ROWNUM := 0 ) B";
+
+        $st = $pdo->prepare($query);
+        $st->execute([$scheduleDate, $kakaoId]);
+    }
+
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return $res;
 }
